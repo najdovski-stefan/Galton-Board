82e70e0a129773a68c719b4b2ba3181bab3af159
Stefan N <9420404-stefan_najdovski@users.noreply.gitlab.com>
2025-04-11 20:37:57 +0200

    added build script, hello world, and ignore files

---
+++ build_run.sh
@@ -0,0 +1,1 @@
+clang -O0 -o galton_board -g -Wall -Wextra src/main.c && ./galton_board
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+
+    write(1,"Hello World!\n",13);
+    return EXIT_SUCCESS;
+}
