5d24d75677ce76888abcc7221bd9534c5d72d9d6
Stefan N <9420404-stefan_najdovski@users.noreply.gitlab.com>
2025-04-11 20:55:22 +0200

    somewhat working Probability Density Function in C

---
+++ README.md
@@ -0,0 +1,12 @@
+# Galton Board
+
+
+## The Normal Distribution
+
+
+
+---
+
+### Resources:
+
+- [Wikipedia on Normal Distribution](https://en.wikipedia.org/wiki/Normal_distribution).
--- src/main.c
+++ src/main.c
@@ -1,8 +1,45 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
+#include <stdio.h>
+
+#define PI 3.1415
+
+float pdf(double mu,double sigma, double x);
+
+float pdf(double mu,double sigma, double x){
+
+return 1/(sigma * sqrt(2*PI)) * exp(-(pow((x-mu),2))/pow((2*sigma),2));
+
+}
+
+/**
+Probability Density Function
+
+f\left(x\right)=\frac{1}{\sigma\cdot\sqrt{2\pi}}\cdot e^{\left(-\frac{\left(x-\mu\right)^{2}}{\left(2\cdot\sigma\right)^{2}}\right)}
+E^-(x-\[Mu])^2/(2 \[Sigma]^2)/(Sqrt[2 \[Pi]] \[Sigma])
+**/
+
 
 int main(){
 
-    write(1,"Hello World!\n",13);
+    double sigma, mu;
+    sigma = 0.2;
+    mu = 0.6;
+
+    double x[1024];
+    for(int i = 0; i < 1024;++i)
+        x[i] = 0.1 + (double)i/10;
+
+
+    for(int i = 0; i < 1024;++i)
+        printf("%f\n",x[i]);
+
+    double pdfResult = pdf(mu,sigma,x[0]);
+    printf("%f\n",pdfResult);
+
+
+
+
     return EXIT_SUCCESS;
 }
